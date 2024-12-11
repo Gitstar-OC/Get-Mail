c9ed1d35e6b34e0715bd18bc1f4b116fe50d9b25
OC <chandankarom07@gmail.com>
2024-12-11 23:58:48 +0530

    Added animations

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,19 @@ export default function Home() {
     <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-4xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
-          <Mail size={72} className="inline-block mb-4 mr-4 text-gray-300 " /> 
-          Get a Mail 
+          <Mail size={72} className="inline-block mb-4 mr-4 text-gray-300 " />
+          Get a Mail
         </h1>
         <p className="text-neutral-400 max-w-lg mx-auto my-4 text-lg text-center relative z-10">
-          Just a simple open source plugin you can use to send email when something happens. 
+          Just a simple {""}
+          <a
+            href="https://github.com/Hackclub-OC/Get-Mail"
+            target="_blank"
+            className="relative before:content-[''] before:absolute before:left-[-0.1px] before:right-[-0.1px] before:h-full before:transition-transform before:duration-[0.6s] before:ease-[cubic-bezier(0.53,0.21,0,1)] text-white before:origin-bottom before:bg-blue-600 before:opacity-60 before:scale-y-[0.3] before:bottom-0 hover:before:scale-y-100"
+          >
+            <span className="relative">open source</span>
+          </a>{" "}
+          plugin you can use to send email when something happens.
         </p>
         <form onSubmit={handleSubmit} className="relative z-10 mt-8">
           <div className="relative flex-grow">
@@ -37,7 +45,14 @@ export default function Home() {
                   placeholder="Email"
                   type="email"
                 />
-                <Button variant="outline">Send</Button>
+                <Button
+                  type="submit"
+                  className="bg-[hsl(222,100%,95%)] text-[hsl(243,80%,62%)] relative overflow-hidden z-[1] px-6 py-3 rounded-md before:content-[''] before:absolute before:w-[140px] before:h-[140px] before:bg-[hsl(243,80%,62%)] before:opacity-0 before:rounded-[50%] before:left-2/4 before:top-2/4 hover:before:opacity-100 hover:before:duration-[0.85s] before:transform before:translate-x-[-50%] before:translate-y-[-50%] before:scale-0 hover:before:scale-[1.2] before:transition-[opacity_0.4s_cubic-bezier(0.19,1,0.22,1),transform_0.75s_cubic-bezier(0.19,1,0.22,1)]"
+                >
+                  <span className="z-[1] relative text-[hsl(222,100%,95%)]">
+                    <Send className="w-4 h-4" />
+                  </span>
+                </Button>
               </div>
             </div>
           </div>
@@ -47,22 +62,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-// .exampleBtn {
-//   @apply bg-[hsl(222,100%,95%)] text-[hsl(243,80%,62%)] relative overflow-hidden z-[1] px-6 py-3 rounded-md before:content-[''] before:absolute before:w-[140px] before:h-[140px] before:bg-[hsl(243,80%,62%)] before:opacity-0 before:rounded-[50%] before:left-2/4 before:top-2/4 hover:before:opacity-100 hover:before:duration-[0.85s];
-// }
-// .exampleBtn span {
-//   @apply z-[1] relative;
-// }
-// .exampleBtn::before {
-//   transform: translate3d(-50%, -50%, 0) scale3d(0, 0, 0);
-//   transition: opacity 0.4s cubic-bezier(0.19, 1, 0.22, 1),
-//     transform 0.75s cubic-bezier(0.19, 1, 0.22, 1);
-// }
-// .exampleBtn:hover span {
-//   @apply text-[hsl(222,100%,95%)];
-// }
-// .exampleBtn:hover::before {
-//   transform: translate3d(-50%, -50%, 0) scale3d(1.2, 1.2, 1.2);
-// }
