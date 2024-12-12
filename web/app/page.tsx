2baf8a74ff5ccd079ff6b7251dc4158c09aadb84
OC <chandankarom07@gmail.com>
2024-12-13 00:10:07 +0530

    Updated page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-// import { BackgroundBeams } from "@/components/ui/background-beams";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Input } from "@/components/ui/input";
 import { Send } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -111,7 +111,7 @@ export default function Home() {
           </div>
         </form>
       </div>
-      {/* <BackgroundBeams /> */}
+      <BackgroundBeams />
     </div>
   );
 }
